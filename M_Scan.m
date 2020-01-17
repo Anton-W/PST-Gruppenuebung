a506bb247d5ae2fc3410d8f3ce546435a9b9b15b
Anton-W <57456985+Anton-W@users.noreply.github.com>
2020-01-17 16:55:28 +0100

    Add files via upload

---
+++ M_Scan.m
@@ -0,0 +1,38 @@
+%##########################################################################
+%
+% Automatisierter Scanablauf für den Farbscanner
+% Bei Aufruf wird die Würfel-Matrix einmal vollständig gescannt
+% Gibt Übersicht über die bestehenden Würfel-Farben zum Programm Beginn
+%
+% PST-Gruppenübung
+% 17.01.2020
+%
+%##########################################################################
+
+function FM = M_Scan(rob)   % Scanner-Position = x + 43.5
+    Rob_move(250,0,65,0,rob);
+        pause(3);
+        p = 2;
+        z = 65;
+        winkel = 0;
+    
+W31 = Scan_Koo(250,60,z,winkel,rob);
+    
+W21 = Scan_Koo(310,60,z,winkel,rob);
+    
+W11 = Scan_Koo(370,60,z,winkel,rob);
+
+W12 = Scan_Koo(370,0,z,winkel,rob);
+    
+W13 = Scan_Koo(370,-60,z,winkel,rob);
+    
+W23 = Scan_Koo(310,-60,z,winkel,rob);
+    
+W33 = Scan_Koo(250,-60,z,winkel,rob);
+    
+W32 = Scan_Koo(250,0,z,winkel,rob);
+ 
+W22 = Scan_Koo(310,0,z,winkel,rob);
+    
+FM = [W11,W12,W13;W21,W22,W23;W31,W32,W33];    
+end
---
+++ Sortieren/tausch.m
@@ -0,0 +1,69 @@
+%##########################################################################
+%INPUT: FARBMATRIX, SOLLMATRIX
+%17.01 
+%Sucht passende Tauschpartner in der Farbmatrix und Tauscht diese Digital
+%und Physisch durch aufrufen der Tauschfunktion.
+%Funktion geht für jeden Matrixwert alle darauf folgenden Farbwerte durch
+%und prüft,ob diese passende Tauschpartner sind. Dabei muss die Erste
+%Position in der Farbmatrix dem Sollwert der Zweiten Position entsprechen
+%und umgekehrt. Die Farbwerte dürfen nicht identisch sein.
+
+
+function tmatrix = tausch(matrix1,matrix2)
+tmatrix=matrix1;
+matrixfeld=[1,2,3;4,5,6;7,8,9];
+%matrix1 Farbmatrix, momentane Anordnung der Würfel
+%matrix2 Sollmatrix, vom User angegeben
+%ROT 1, BLAU 2, GRÜN 3
+for zeile=1:3 
+    for spalte=1:3
+        
+        ist = matrix1(zeile,spalte); %Istwert 1. Position
+        soll = matrix2(zeile,spalte); %Sollwert 1. Position
+        
+        for zeile2=0+zeile:3
+            for spalte2=0+spalte:3
+                
+                ist2=matrix1(zeile2,spalte2); %Istwert 2.Position
+                soll2=matrix2(zeile2,spalte2); %Sollwert 2.Position
+                
+                if ist==soll2 && soll==ist2 && matrix1(zeile,spalte)~=matrix1(zeile2,spalte2) 
+                  %Vergleich der Position 1 und 2 und prüfung ob Zahlen
+                  %identisch sind
+                  %disp([num2str([zeile,spalte]),' Tauschbar mit ',num2str([zeile2,spalte2])]);
+   
+                   placeholder = matrix1(zeile,spalte);
+                   placeholder2 = matrix1(zeile2,spalte2);
+                   tmatrix(zeile2,spalte2)=placeholder;
+                   tmatrix(zeile,spalte)=placeholder2;
+                   %Speicherung der Tauschung in neuer Matrix (tmatrix)
+                   
+                 
+                   
+                   W1 = matrixfeld(zeile,spalte);
+                   W2 = matrixfeld(zeile2,spalte2);
+                   disp([num2str(W1),' mit ',num2str(W2)]);
+                  % disp(tmatrix);
+                  % disp('----');
+                  
+                   %HIER VROB TAUSCHFUNKTION EINFÜGEN
+                   
+                   tausch(tmatrix,matrix2);
+                   %Rekursiver Algorithmus, ist kein Tauschpartner
+                   %vorhanden wir die Funktion nach dem letzten Tausch
+                   %nicht erneut aufgerufen
+                   
+                   return
+                   
+                end
+                
+                
+            end
+        end
+        
+    end
+end
+
+
+
+end
